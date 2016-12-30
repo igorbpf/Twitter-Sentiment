e7481b152a59cc0f210d12d3f4d1634e501c0ab3
Igor Santos <igorbpf@gmail.com>
2016-12-30 16:43:05 -0200

    Test app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.svm import SVC
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    x = np.array([[1., 2.], [3., 2.]])
-    y = np.array([[3., 2.], [1., 1.]])
-    return str(x * y)
+    iris = load.iris()
+    X, y = iris.data, iris.target
+    svm = SVC(kernel='linear')
+    svm.fit(X)
+    y_pred = svm.predict(X)
+
+    return jsonify(target=y_pred)
 
 if __name__ == '__main__':
     app.run(debug=True)
