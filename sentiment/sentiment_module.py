4b68ef2c35e5eff6b39a15a9e0378356c215eb72
Igor Santos <igorbpf@gmail.com>
2016-12-31 21:00:59 -0200

    Make test without stopwords

--- sentiment/sentiment_module.py
+++ sentiment/sentiment_module.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import nltk
+#import nltk
 #from nltk.corpus import stopwords
 #from nltk.stem import RSLPStemmer
 import re
@@ -14,9 +14,9 @@ def review_to_words(review):
     letters_only = re.sub("\W+", " ", review, flags=re.UNICODE)
 
     words = letters_only.lower().split()
-    nltk.data.path.append('./nltk_data/')
-    stops = set(nltk.corpus.stopwords.words("portuguese"))
-    meaningful_words = [w for w in words if not w in stops]
+    #nltk.data.path.append('./nltk_data/')
+    #stops = set(nltk.corpus.stopwords.words("portuguese"))
+    meaningful_words = words #[w for w in words if not w in stops]
     #stemmer = RSLPStemmer()
     meaningful_stemmed = meaningful_words #[stemmer.stem(w) for w in meaningful_words]
     return(" ".join(meaningful_stemmed))
